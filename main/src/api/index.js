d08e4518abcca0b52a969d5f47667ea229a00fd3
isaac <irrperks@gmail.com>
2022-04-10 06:08:55 -0700

    added basic routing code for express, this file will contain all access points between app and client. Any middleware can be imported into here. Code untested and is a simple foundation

--- main/src/api/index.js
+++ main/src/api/index.js
@@ -1,1 +1,23 @@
-// JavaScript source code
+/*
+Auther: Isaac Perks
+Date: 04/09/2022
+Description:
+API code to be exported goes here. This will contain server related code for going between client and app.js Should contain main webpage info, routes, responses etc...
+*/
+
+require('../express')
+const server = express()
+
+server.get('/api/test', (req, res) => {
+    res.send({
+        message:'hello'
+    })
+})
+
+server.post('/api/register', (req, res) => {
+    res.send({
+        message:'test'
+    })
+})
+
+server.listen(process.env.PORT || 8081)
