2b1ef55602e11c977764ae9ad51f892653573e09
Najir <irrperks@gmail.com>
2022-05-22 17:43:43 -0700

    Added static file paths and client access. Added boilerplate for database CRUD access.

--- main/src/api/index.js
+++ main/src/api/index.js
@@ -7,15 +7,16 @@ This will contain server related code for going between client and app.js Should
 
 const express = require('express')
 const server = express()
-const app = require('./app.js')
+const appClient = require('../app.js')
+const path = require('path')
 
+server.use('/paths', express.static(path.join(__dirname, '..', 'client', 'routes')))
+server.use('/static', express.static(path.join(__dirname, '..', 'client', 'components')))
 
 server.get('/', (req, res) => {
-    res.send({
-        message: 'hello'
-    })
-    res.sendFile('./client/mainpage.html')
+    res.sendFile(path.join(__dirname, '..', 'client', 'mainpage.html'))
 })
+
 server.get('/api/test', (req, res) => {
     res.send({
         message:'hello'
@@ -29,8 +30,32 @@ server.post('/api/register', (req, res) => {
 })
 
 server.get('/api/user/:id', (req, res) => {
+    res.json(appClient.steamFetch(req.params.id));
+})
+
+server.get('/database/read/:id', (req, res) => {
     res.send({
-        app.steamFetch(req.params.id)
+
+    })
+
+})
+
+server.put('/database/create/:id', (req, res) => {
+    res.send({
+
+    })
+
+})
+
+server.post('/database/update/:id', (req, res) => {
+    res.send({
+
+    })
+})
+
+server.get('/database/delete/:id', (req, res) => {
+    res.send({
+
     })
 })
 
